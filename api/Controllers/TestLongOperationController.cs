02afa19783535cb60bbf51b8068dfcbad12a315c
thealiev <tarqymuhammadal@gmail.com>
2023-12-30 20:19:07 +0500

    create new project

---
+++ api/Controllers/TestLongOperationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestTimeConsumingOperationWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestLongOperationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(5000);
+
+            return Content("Web API Long Operation Completed");
+        }
+    }
+}
